2526d58cb26d909beca0fd41485316f968721d64
giseong ji <jiggyjiggy0323@gmail.com>
2022-08-28 23:39:47 +0900

    refactor: Fix calculator test
    ParameterizedTest를 사용하여 하나로 통합

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,34 +2,42 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 /*
-* 구현 사항
-* - 사칙연산
-* - 양수로만 계산
-* - 나눗셈에서 0으로 나누는 경우 IllegalArgument 예외를 발생시킨다
-* - MVC(Model-View-Controller) pattern 으로 구현한다
-* */
+ * 구현 사항
+ * - 사칙연산
+ * - 양수로만 계산
+ * - 나눗셈에서 0으로 나누는 경우 IllegalArgument 예외를 발생시킨다
+ * - MVC(Model-View-Controller) pattern 으로 구현한다
+ * */
 public class CalculatorTest {
 
     // 1 + 2 ------> Calculator
     //   3   <------
-    @DisplayName("덧셈 연산을 정상적으로 수행한다")
-    @Test
-    void additionTest() {
-        int result = Calculator.calculator(1, "+", 2);
+    @DisplayName("사칙 연산을 정상적으로 수행한다")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void additionTest(int operand1, String operator, int operand2, int result) {
+        int calculatorResult = Calculator.calculator(operand1, operator, operand2);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculatorResult).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산을 정상적으로 수행한다")
-    @Test
-    void subtrationTest() {
-        int result = Calculator.calculator(1, "-", 2);
-
-        assertThat(result).isEqualTo(-1);
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
     }
 }
